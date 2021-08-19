7bcb376b4cb5e31fd19ea9fdabf7a76080e55a1c
andres.seoane <andres.seoane@intive.com>
2021-08-19 11:13:28 -0300

    UI for home (popular screenings)

--- app/src/main/java/com/intive/tmdbandroid/home/ui/HomeFragment.kt
+++ app/src/main/java/com/intive/tmdbandroid/home/ui/HomeFragment.kt
@@ -1,15 +1,23 @@
-package com.feragusper.eventbritelite.ui
+package com.intive.tmdbandroid.home.ui
 
+import android.graphics.LinearGradient
+import android.graphics.Shader
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.intive.tmdbandroid.R
 import com.intive.tmdbandroid.common.state.Resource
 import com.intive.tmdbandroid.databinding.FragmentHomeBinding
+import com.intive.tmdbandroid.home.ui.adapters.MovieAdapter
+import com.intive.tmdbandroid.home.ui.adapters.TVShowAdapter
 import com.intive.tmdbandroid.home.viewmodel.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
@@ -19,6 +27,9 @@ class HomeFragment : Fragment() {
 
     private val viewModel: HomeViewModel by viewModels()
 
+    private val movieAdapter = MovieAdapter(arrayListOf())
+    private val tvShowsAdapter = TVShowAdapter(arrayListOf())
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -27,7 +38,9 @@ class HomeFragment : Fragment() {
         val binding = FragmentHomeBinding.inflate(inflater, container, false)
         context ?: return binding.root
 
-        subscribeUi(binding)
+        initViews(binding)
+//        subscribeUi(binding)
+        subscribePopularData(binding)
 
         return binding.root
     }
@@ -36,10 +49,11 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel.sample()
+        viewModel.popularMovies()
+        viewModel.popularTVShows()
     }
 
-
-    private fun subscribeUi(binding: FragmentHomeBinding) {
+    /*private fun subscribeUi(binding: FragmentHomeBinding) {
         lifecycleScope.launchWhenStarted {
             viewModel.sampleFlow.collectLatest { sampleResource ->
                 when (sampleResource.status) {
@@ -50,15 +64,101 @@ class HomeFragment : Fragment() {
                         Toast.makeText(context, sampleResource.message, Toast.LENGTH_LONG).show()
                     }
                     Resource.Status.LOADING -> {
-
                     }
                     Resource.Status.FAILURE -> {
                         Toast.makeText(context, sampleResource.message, Toast.LENGTH_LONG).show()
                     }
                 }
+            }
+        }
+    }*/
+
+    private fun subscribePopularData(binding: FragmentHomeBinding) {
+        lifecycleScope.launchWhenStarted {
+            viewModel.popularMoviesFlow.collectLatest { resultMovies ->
+                Log.i("MAS", "popular movies status: ${resultMovies.status}")
+
+                when (resultMovies.status) {
+                    Resource.Status.SUCCESS -> {
+                        binding.moviesProgress.visibility = View.GONE
+                        resultMovies.data?.movies?.let { movieAdapter.refresh(it) }
+                    }
+                    Resource.Status.ERROR -> {
+                        binding.moviesProgress.visibility = View.GONE
+                        Toast.makeText(context, resultMovies.message, Toast.LENGTH_LONG).show()
+                    }
+                    Resource.Status.LOADING -> {
+                        binding.moviesProgress.visibility = View.VISIBLE
+                    }
+                    Resource.Status.FAILURE -> {
+                        binding.moviesProgress.visibility = View.GONE
+                        Toast.makeText(context, resultMovies.message, Toast.LENGTH_LONG).show()
+                    }
+                }
 
             }
         }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.popularTVShowsFlow.collectLatest { resultTVShows ->
+                Log.i("MAS", "popular tvshows status: ${resultTVShows.status}")
+
+                when (resultTVShows.status) {
+                    Resource.Status.SUCCESS -> {
+                        binding.tvshowsProgress.visibility = View.GONE
+                        resultTVShows.data?.TVShows?.let { tvShowsAdapter.refresh(it) }
+                    }
+                    Resource.Status.ERROR -> {
+                        binding.tvshowsProgress.visibility = View.GONE
+                        Toast.makeText(context, resultTVShows.message, Toast.LENGTH_LONG).show()
+                    }
+                    Resource.Status.LOADING -> {
+                        binding.tvshowsProgress.visibility = View.VISIBLE
+                    }
+                    Resource.Status.FAILURE -> {
+                        binding.tvshowsProgress.visibility = View.GONE
+                        Toast.makeText(context, resultTVShows.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun initViews(binding: FragmentHomeBinding) {
+        val rvTopMovies = binding.rvPopularMovies
+        val rvTopTVShows = binding.rvPopularTVShows
+
+        rvTopMovies.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            adapter = movieAdapter
+        }
+        rvTopTVShows.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            adapter = tvShowsAdapter
+        }
+
+        val topMoviesText = binding.popularMoviesText
+        val topTVShowsText = binding.popularTvshowsText
+
+        val moviesTextwidth = topMoviesText.paint.measureText(topMoviesText.text.toString())
+        val tvShowsTextwidth = topTVShowsText.paint.measureText(topMoviesText.text.toString())
+
+        val darkseaGreen = ContextCompat.getColor(binding.root.context, R.color.darksea_green)
+        val lightseaGreen = ContextCompat.getColor(binding.root.context, R.color.lightsea_green)
+
+        val moviesTextShader = LinearGradient(0f, 0f, moviesTextwidth, 0f,
+            darkseaGreen,
+            lightseaGreen,
+            Shader.TileMode.CLAMP)
+        val tvShowsTextShader = LinearGradient(0f, 0f, tvShowsTextwidth, 0f,
+            darkseaGreen,
+            lightseaGreen,
+            Shader.TileMode.CLAMP)
+
+        topMoviesText.setTextColor(darkseaGreen)
+        topMoviesText.paint.shader = moviesTextShader
+        topTVShowsText.setTextColor(darkseaGreen)
+        topTVShowsText.paint.shader = tvShowsTextShader
     }
 
 }
--- app/src/main/java/com/intive/tmdbandroid/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/intive/tmdbandroid/home/viewmodel/HomeViewModel.kt
@@ -3,8 +3,12 @@ package com.intive.tmdbandroid.home.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.intive.tmdbandroid.common.state.Resource
+import com.intive.tmdbandroid.model.ResultMovies
+import com.intive.tmdbandroid.model.ResultTVShows
 import com.intive.tmdbandroid.sample.model.Sample
 import com.intive.tmdbandroid.sample.usecase.SampleUseCase
+import com.intive.tmdbandroid.usecase.PopularMoviesUseCase
+import com.intive.tmdbandroid.usecase.PopularTVShowsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.catch
@@ -16,10 +20,18 @@ import javax.inject.Inject
 @HiltViewModel
 class HomeViewModel @Inject internal constructor(
     private val sampleUseCase: SampleUseCase,
+    private val popularMoviesUseCase: PopularMoviesUseCase,
+    private val popularTVShowsUseCase: PopularTVShowsUseCase
 ) : ViewModel() {
 
     private val _sampleFlow = Channel<Resource<Sample>>(Channel.BUFFERED)
     val sampleFlow = _sampleFlow.receiveAsFlow()
+
+    private val _popularMoviesFlow = Channel<Resource<ResultMovies>>(Channel.BUFFERED)
+    val popularMoviesFlow = _popularMoviesFlow.receiveAsFlow()
+
+    private val _popularTVShowsFlow = Channel<Resource<ResultTVShows>>(Channel.BUFFERED)
+    val popularTVShowsFlow = _popularTVShowsFlow.receiveAsFlow()
 
     fun sample() {
         viewModelScope.launch {
@@ -32,6 +44,34 @@ class HomeViewModel @Inject internal constructor(
                 .collect { sample ->
                     _sampleFlow.send(Resource.success(sample))
 
+                }
+        }
+    }
+
+    fun popularMovies() {
+        viewModelScope.launch {
+            _popularMoviesFlow.send(Resource.loading())
+
+            popularMoviesUseCase()
+                .catch { e ->
+                    _popularMoviesFlow.send(Resource.error(e.toString()))
+                }
+                .collect { resultMovies ->
+                    _popularMoviesFlow.send(Resource.success(resultMovies))
+                }
+        }
+    }
+
+    fun popularTVShows() {
+        viewModelScope.launch {
+            _popularTVShowsFlow.send(Resource.loading())
+
+            popularTVShowsUseCase()
+                .catch { e ->
+                    _popularTVShowsFlow.send(Resource.error(e.toString()))
+                }
+                .collect { resultTVShows ->
+                    _popularTVShowsFlow.send(Resource.success(resultTVShows))
                 }
         }
     }
