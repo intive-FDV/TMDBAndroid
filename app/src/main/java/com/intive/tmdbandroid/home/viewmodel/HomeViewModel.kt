2b99a6553761a75bd8747761fcc77b14f0256406
andres.seoane <andres.seoane@intive.com>
2021-08-24 14:08:10 -0300

    Removed comments and unusable code

--- app/src/main/java/com/intive/tmdbandroid/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/intive/tmdbandroid/home/viewmodel/HomeViewModel.kt
@@ -6,8 +6,6 @@ import androidx.lifecycle.viewModelScope
 import com.intive.tmdbandroid.common.state.Resource
 import com.intive.tmdbandroid.entity.ResultMovies
 import com.intive.tmdbandroid.entity.ResultTVShows
-//import com.intive.tmdbandroid.sample.model.Sample
-import com.intive.tmdbandroid.sample.usecase.SampleUseCase
 import com.intive.tmdbandroid.usecase.PopularMoviesUseCase
 import com.intive.tmdbandroid.usecase.PopularTVShowsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -20,34 +18,15 @@ import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject internal constructor(
-    private val sampleUseCase: SampleUseCase,
     private val popularMoviesUseCase: PopularMoviesUseCase,
     private val popularTVShowsUseCase: PopularTVShowsUseCase
 ) : ViewModel() {
-
-//    private val _sampleFlow = Channel<Resource<Sample>>(Channel.BUFFERED)
-//    val sampleFlow = _sampleFlow.receiveAsFlow()
 
     private val _popularMoviesFlow = Channel<Resource<ResultMovies>>(Channel.BUFFERED)
     val popularMoviesFlow = _popularMoviesFlow.receiveAsFlow()
 
     private val _popularTVShowsFlow = Channel<Resource<ResultTVShows>>(Channel.BUFFERED)
     val popularTVShowsFlow = _popularTVShowsFlow.receiveAsFlow()
-
-    /*fun sample() {
-        viewModelScope.launch {
-            _sampleFlow.send(Resource.loading())
-
-            sampleUseCase()
-                .catch { e ->
-                    _sampleFlow.send(Resource.error(e.toString()))
-                }
-                .collect { sample ->
-                    _sampleFlow.send(Resource.success(sample))
-
-                }
-        }
-    }*/
 
     fun popularMovies() {
         viewModelScope.launch {
