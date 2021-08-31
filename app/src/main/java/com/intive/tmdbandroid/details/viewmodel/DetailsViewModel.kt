d5f0ab4e3fefcdb75c145e1858c469ad95fe86d6
Francisco Beccuti <fbeccuti@synapticlinks.com.ar>
2021-08-31 10:05:42 -0300

    TV Show Detail *Model View *Use Case

--- app/src/main/java/com/intive/tmdbandroid/details/viewmodel/DetailsViewModel.kt
+++ app/src/main/java/com/intive/tmdbandroid/details/viewmodel/DetailsViewModel.kt
@@ -1,4 +1,40 @@
 package com.intive.tmdbandroid.details.viewmodel
 
-class DetailsViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.intive.tmdbandroid.model.TVShow
+import com.intive.tmdbandroid.usecase.DetailTVShowsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailsViewModel @Inject internal constructor(
+    private val tVShowsUseCase: DetailTVShowsUseCase) : ViewModel() {
+
+    private val _state = MutableStateFlow<State>(State.Loading)
+
+    val uiState: StateFlow<State> = _state
+
+    fun tVShows(id:Int) {
+        viewModelScope.launch {
+            tVShowsUseCase(id)
+                .catch { e ->
+                    _state.value = State.Error(e)
+                }
+                .collect { TVShow ->
+                    _state.value = State.Success(TVShow)
+                }
+        }
+    }
+}
+
+sealed class State {
+    object Loading : State()
+    data class Success(val data: TVShow) : State()
+    data class Error(val exception: Throwable) : State()
 }
