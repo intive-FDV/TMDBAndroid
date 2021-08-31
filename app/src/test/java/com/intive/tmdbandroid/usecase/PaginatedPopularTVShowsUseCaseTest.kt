fe1be59621482c6132b51d8775a8dcf896e59877
urielgarrido <uriel.garrido@intive.com>
2021-08-31 16:35:01 -0300

    Add Unit Test for DetailsViewModel and DetailTvShowUseCase

--- app/src/test/java/com/intive/tmdbandroid/usecase/PaginatedPopularTVShowsUseCaseTest.kt
+++ app/src/test/java/com/intive/tmdbandroid/usecase/PaginatedPopularTVShowsUseCaseTest.kt
@@ -3,14 +3,14 @@ package com.intive.tmdbandroid.usecase
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.paging.PagingData
 import app.cash.turbine.test
-import com.google.common.truth.Truth
 import com.intive.tmdbandroid.common.MainCoroutineRule
+import com.intive.tmdbandroid.model.Genre
 import com.intive.tmdbandroid.model.TVShow
 import com.intive.tmdbandroid.repository.CatalogRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.runBlockingTest
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -31,17 +31,19 @@ class PaginatedPopularTVShowsUseCaseTest{
             TVShow(
                 backdrop_path = "BACKDROP_PATH",
                 first_air_date = "1983-10-20",
-                genre_ids = listOf(1, 2),
+                genres = listOf(Genre(1, "genre1"), Genre(2,"genre2")),
                 id = 1,
                 name = "Simona la Cacarisa",
-                origin_country = listOf("AR"),
-                original_language = "es",
                 original_name = "El cochiloco",
                 overview = "Simona la cacarisa, el cochiloco",
-                popularity = 20.2,
                 poster_path = "POSTER_PATH",
                 vote_average = 10.5,
-                vote_count = 100
+                vote_count = 100,
+                created_by = emptyList(),
+                last_air_date = "1990-09-25",
+                number_of_episodes = 5,
+                number_of_seasons = 2,
+                status = "Online"
             )
         )
     )
