dd7d7cc587a2895eb27a2861dfc8c3cfa257dbb9
urielgarrido <uriel.garrido@intive.com>
2021-09-02 14:45:20 -0300

    Add fragment_search.xml and I add simple logic on SearchFragment.kt

--- app/src/main/java/com/intive/tmdbandroid/search/ui/SearchFragment.kt
+++ app/src/main/java/com/intive/tmdbandroid/search/ui/SearchFragment.kt
@@ -1,4 +1,33 @@
 package com.intive.tmdbandroid.search.ui
 
-class SearchFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.intive.tmdbandroid.databinding.FragmentSearchBinding
+import com.intive.tmdbandroid.search.viewmodel.SearchViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SearchFragment: Fragment() {
+    private val viewModel: SearchViewModel by viewModels()
+
+    private lateinit var binding: FragmentSearchBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.search()
+    }
 }
