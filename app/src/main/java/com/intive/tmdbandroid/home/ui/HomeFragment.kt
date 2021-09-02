8b80ca9d2217b5e5bacff6ac917610578d838c2e
Francisco Beccuti <fbeccuti@synapticlinks.com.ar>
2021-09-02 09:33:45 -0300

    Loading *Add progress bar to loading.

--- app/src/main/java/com/intive/tmdbandroid/home/ui/HomeFragment.kt
+++ app/src/main/java/com/intive/tmdbandroid/home/ui/HomeFragment.kt
@@ -15,6 +15,8 @@ import com.intive.tmdbandroid.home.viewmodel.HomeViewModel
 import com.intive.tmdbandroid.home.viewmodel.State
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
@@ -42,8 +44,9 @@ class HomeFragment : Fragment() {
     }
 
     private fun subscribePopularData(binding: FragmentHomeBinding) {
-        lifecycleScope.launchWhenStarted {
-            viewModel.uiState.collect { resultTVShows ->
+        binding.layoutProgressbar.progressBar.visibility = View.VISIBLE
+        lifecycleScope.launch {
+            viewModel.uiState.collectLatest { resultTVShows ->
                 Log.i("MAS", "popular tvshows status: $resultTVShows")
 
                 when (resultTVShows) {
@@ -51,7 +54,6 @@ class HomeFragment : Fragment() {
                         binding.layoutError.errorContainer.visibility = View.GONE
                         binding.layoutProgressbar.progressBar.visibility = View.GONE
                         tvShowPageAdapter.submitData(resultTVShows.data)
-
                         if (tvShowPageAdapter.itemCount == 0) {
                             binding.layoutEmpty.root.visibility = View.VISIBLE
                         } else binding.layoutEmpty.root.visibility = View.GONE
