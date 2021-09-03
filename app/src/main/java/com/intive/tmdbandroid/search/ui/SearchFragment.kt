9c143c8d5f1ccb40404e1f6aade5e416481bf984
Juan Dominguez <j.dominguez@intive.com>
2021-09-03 19:43:20 -0300

    Add toolbar with navigation to Search Fragment

--- app/src/main/java/com/intive/tmdbandroid/search/ui/SearchFragment.kt
+++ app/src/main/java/com/intive/tmdbandroid/search/ui/SearchFragment.kt
@@ -5,14 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.AppBarLayout
 import com.intive.tmdbandroid.databinding.FragmentSearchBinding
-import com.intive.tmdbandroid.search.viewmodel.SearchViewModel
+import com.intive.tmdbandroid.search.ui.adapters.TVShowSearchAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SearchFragment: Fragment() {
 //    private val viewModel: SearchViewModel by viewModels()
+
+    private val searchAdapter = TVShowSearchAdapter()
 
     private lateinit var binding: FragmentSearchBinding
 
@@ -23,11 +28,20 @@ class SearchFragment: Fragment() {
     ): View {
         binding = FragmentSearchBinding.inflate(inflater, container, false)
 
+        println(searchAdapter.toString())
         return binding.root
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupToolbar()
 //        viewModel.search()
+    }
+    private fun setupToolbar() {
+        val navController = findNavController()
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        val toolbar = binding.fragmentSearchToolbar
+        toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
