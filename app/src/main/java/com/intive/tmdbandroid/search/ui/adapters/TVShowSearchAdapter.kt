230db80a93c7e726888fcf2b1f5a9ced15820ae2
Juan Dominguez <j.dominguez@intive.com>
2021-09-06 16:20:19 -0300

    Add Search Adapter skeleton

---
+++ app/src/main/java/com/intive/tmdbandroid/search/ui/adapters/TVShowSearchAdapter.kt
@@ -0,0 +1,84 @@
+package com.intive.tmdbandroid.search.ui.adapters
+
+import android.content.res.ColorStateList
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.os.bundleOf
+import androidx.navigation.findNavController
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.intive.tmdbandroid.R
+import com.intive.tmdbandroid.databinding.ItemFoundSearchBinding
+import com.intive.tmdbandroid.model.TVShow
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+class TVShowSearchAdapter : PagingDataAdapter<TVShow, TVShowSearchAdapter.TVShowHolder>(REPO_COMPARATOR) {
+    companion object {
+        private val REPO_COMPARATOR = object : DiffUtil.ItemCallback<TVShow>() {
+            override fun areItemsTheSame(oldItem: TVShow, newItem: TVShow): Boolean = (oldItem == newItem)
+            override fun areContentsTheSame(oldItem: TVShow, newItem: TVShow): Boolean = (oldItem == newItem)
+        }
+    }
+
+    override fun onBindViewHolder(holder: TVShowHolder, position: Int) {
+        with(holder) {
+            with(getItem(position) as TVShow) {
+                val options = RequestOptions()
+                    .centerCrop()
+                    .placeholder(R.drawable.ic_image)
+                    .error(R.drawable.ic_image)
+
+                val posterURL = binding.root.resources.getString(R.string.base_imageURL) + poster_path
+
+                val context = binding.root.context
+
+                Glide.with(context)
+                    .load(posterURL)
+                    .apply(options)
+                    .into(binding.itemPosterSearch)
+
+                val percentage = (vote_average * 10).toInt()
+
+                try {
+                    val date = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).parse(first_air_date)
+                    val stringDate = SimpleDateFormat("MMM dd, yyyy", Locale.getDefault()).format(date!!)
+                    binding.itemYearSearch.text = stringDate
+                } catch (e: Exception) {
+                    binding.itemYearSearch.text = ""
+                }
+
+//                binding.itemRatingSearch.progress = percentage
+
+//                when {
+//                    percentage < 25 -> binding.circularPercentage.progressTintList = ColorStateList.valueOf(
+//                        ContextCompat.getColor(context, R.color.red))
+//                    percentage < 45 -> binding.circularPercentage.progressTintList = ColorStateList.valueOf(
+//                        ContextCompat.getColor(context, R.color.orange))
+//                    percentage < 75 -> binding.circularPercentage.progressTintList = ColorStateList.valueOf(
+//                        ContextCompat.getColor(context, R.color.yellow))
+//                    else -> binding.circularPercentage.progressTintList = ColorStateList.valueOf(
+//                        ContextCompat.getColor(context, R.color.green))
+//                }
+
+                binding.itemRatingSearch.numStars = percentage
+                binding.itemTitleSearch.text = name
+
+                binding.root.setOnClickListener() {
+                    it.findNavController().navigate(R.id.action_homeFragmentDest_to_TVShowDetail, bundleOf("id" to id, "screeningTitle" to name))
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TVShowHolder = TVShowHolder(
+        ItemFoundSearchBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    )
+
+    inner class TVShowHolder (val binding: ItemFoundSearchBinding) : RecyclerView.ViewHolder(binding.root)
+}
