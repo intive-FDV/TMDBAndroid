706164e7e9a74489d7ba534b917a19f5edee0c84
Juan Dominguez <j.dominguez@intive.com>
2021-09-07 18:06:49 -0300

    Add subscribe and init methods when searchView is submitted

--- app/src/main/java/com/intive/tmdbandroid/search/ui/SearchFragment.kt
+++ app/src/main/java/com/intive/tmdbandroid/search/ui/SearchFragment.kt
@@ -1,24 +1,34 @@
 package com.intive.tmdbandroid.search.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import androidx.paging.PagingData
+import androidx.recyclerview.widget.GridLayoutManager
+import com.intive.tmdbandroid.common.State
 import com.intive.tmdbandroid.databinding.FragmentSearchBinding
+import com.intive.tmdbandroid.model.TVShow
+import com.intive.tmdbandroid.search.ui.adapters.TVShowSearchAdapter
 import com.intive.tmdbandroid.search.viewmodel.SearchViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
 @AndroidEntryPoint
 class SearchFragment: Fragment() {
-    // private val viewModel: SearchViewModel by viewModels()
+    private val viewModel: SearchViewModel by viewModels()
 
-    // private val searchAdapter = SearchViewModel()
+    private val searchList = arrayListOf<TVShow>()
+
+    private val searchAdapter = TVShowSearchAdapter(searchList)
 
     private lateinit var binding: FragmentSearchBinding
 
@@ -30,14 +40,31 @@ class SearchFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentSearchBinding.inflate(inflater, container, false)
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
 
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String): Boolean {
+                if (query.isNotEmpty()){
+                    viewModel.search(query)
+                    subscribeViewModel()
+                    initViews()
+                    println("ya busque")
+                    return true
+                }
+                return false
+            }
+
+        })
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupToolbar()
-        //viewModel.search()
+
     }
 
     private fun setupToolbar() {
@@ -46,5 +73,34 @@ class SearchFragment: Fragment() {
         val toolbar = binding.fragmentSearchToolbar
         toolbar.setupWithNavController(navController, appBarConfiguration)
         binding.searchView.requestFocus()
+    }
+
+    fun subscribeViewModel(){
+        lifecycleScope.launchWhenStarted {
+            viewModel.uiState.collectLatest { resultTVShow ->
+
+                when (resultTVShow) {
+                    is State.Success<List<TVShow>> -> {
+                        searchAdapter.refresh(resultTVShow.data)
+                        println("ta bien")
+                    }
+                    is State.Error -> {
+                        println("ta mal")
+                    }
+                    is State.Loading -> {
+                        println("ta cargando")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun initViews() {
+        val resultsList = binding.searchResults
+
+        resultsList.apply {
+            layoutManager = GridLayoutManager(context, 1, GridLayoutManager.VERTICAL, false)
+            adapter = searchAdapter
+        }
     }
 }
