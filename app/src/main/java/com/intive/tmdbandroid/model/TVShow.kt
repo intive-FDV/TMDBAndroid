457f78736d60f9ef16dbcd23510ad6e1df16bd33
andres.seoane <andres.seoane@intive.com>
2021-09-13 14:05:18 -0300

    Undo merge with room

--- app/src/main/java/com/intive/tmdbandroid/model/TVShow.kt
+++ app/src/main/java/com/intive/tmdbandroid/model/TVShow.kt
@@ -1,20 +1,10 @@
 package com.intive.tmdbandroid.model
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-import androidx.room.TypeConverters
-import com.intive.tmdbandroid.model.converter.CreatedByConverter
-
-
-@Entity
 data class TVShow(
-
     val backdrop_path: String?,
-    @TypeConverters(CreatedByConverter::class)
     val created_by: List<CreatedBy>,
     val first_air_date: String?,
     val genres: List<Genre>,
-    @PrimaryKey(autoGenerate = false)
     val id: Int,
     val last_air_date: String?,
     val name: String,
