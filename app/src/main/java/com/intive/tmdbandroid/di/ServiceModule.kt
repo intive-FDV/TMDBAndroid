41d7cb199dd8fd004eaf7b4ecb47736b7442f67a
Francisco Beccuti <fbeccuti@synapticlinks.com.ar>
2021-09-14 13:22:33 -0300

    Repo + data source + room code modification to use viewModel

--- app/src/main/java/com/intive/tmdbandroid/di/ServiceModule.kt
+++ app/src/main/java/com/intive/tmdbandroid/di/ServiceModule.kt
@@ -1,5 +1,10 @@
 package com.intive.tmdbandroid.di
 
+import android.content.Context
+import androidx.room.Room
+import com.intive.tmdbandroid.common.Constants
+import com.intive.tmdbandroid.datasource.local.Dao
+import com.intive.tmdbandroid.datasource.local.LocalStorage
 import com.intive.tmdbandroid.datasource.network.Service
 import dagger.Module
 import dagger.Provides
@@ -15,4 +20,19 @@ class ServiceModule {
     fun provideEventAPIService() : Service {
         return Service()
     }
+
+    @Provides
+    @Singleton
+    fun provideDatabase(context: Context): LocalStorage = Room.databaseBuilder(
+        context,
+        LocalStorage::class.java, Constants.DATABASE
+    )
+        .fallbackToDestructiveMigration()
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideDao(
+        database: LocalStorage
+    ): Dao = database.tvShowDao()
 }
