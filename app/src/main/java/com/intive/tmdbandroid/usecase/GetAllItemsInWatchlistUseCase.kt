7f5a3277eab43156d3cb766f512a9b7b82c43659
Juan Dominguez <j.dominguez@intive.com>
2021-09-17 13:03:36 -0300

    Remove unused imports

--- app/src/main/java/com/intive/tmdbandroid/usecase/GetAllItemsInWatchlistUseCase.kt
+++ app/src/main/java/com/intive/tmdbandroid/usecase/GetAllItemsInWatchlistUseCase.kt
@@ -1,10 +1,8 @@
 package com.intive.tmdbandroid.usecase
 
-import com.intive.tmdbandroid.entity.TVShowORMEntity
 import com.intive.tmdbandroid.model.TVShow
 import com.intive.tmdbandroid.repository.CatalogRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
 class GetAllItemsInWatchlistUseCase @Inject constructor(private val catalogRepository: CatalogRepository) {
