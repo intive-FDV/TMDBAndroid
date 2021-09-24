3f04d876fc63953c0a14d071f391f7137b77c5af
urielgarrido <uriel.garrido@intive.com>
2021-09-24 12:24:04 -0300

    Change the mapping method

--- app/src/main/java/com/intive/tmdbandroid/entity/ResultTVShowsEntity.kt
+++ app/src/main/java/com/intive/tmdbandroid/entity/ResultTVShowsEntity.kt
@@ -2,7 +2,6 @@ package com.intive.tmdbandroid.entity
 
 import com.google.gson.annotations.SerializedName
 import com.intive.tmdbandroid.model.Screening
-import com.intive.tmdbandroid.model.TVShow
 
 data class ResultTVShowsEntity(
     val page: Int,
@@ -12,9 +11,7 @@ data class ResultTVShowsEntity(
     val total_results: Int
 ) {
     fun toScreeningList() : List<Screening> {
-        val tvShowList : ArrayList<TVShow> = arrayListOf()
-        TVShows.mapTo(tvShowList, { it.toTVShow() })
         val screeningList: ArrayList<Screening> = arrayListOf()
-        return tvShowList.mapTo(screeningList,{it.toScreening()})
+        return TVShows.mapTo(screeningList,{it.toScreening()})
     }
 }
