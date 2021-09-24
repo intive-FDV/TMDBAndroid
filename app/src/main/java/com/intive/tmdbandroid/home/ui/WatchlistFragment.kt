8ba48811db0cb3cf8a762b3b07c9f57a823d05b5
andres.seoane <andres.seoane@intive.com>
2021-09-24 15:42:34 -0300

    Removed popular TV Shows from WatchlistFragment.kt Modified WatchlistAdapter.kt to use new ItemScreenengSmallBinding

--- app/src/main/java/com/intive/tmdbandroid/home/ui/WatchlistFragment.kt
+++ app/src/main/java/com/intive/tmdbandroid/home/ui/WatchlistFragment.kt
@@ -4,16 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.findNavController
-import androidx.paging.PagingData
 import androidx.recyclerview.widget.GridLayoutManager
 import com.intive.tmdbandroid.R
 import com.intive.tmdbandroid.common.State
 import com.intive.tmdbandroid.databinding.FragmentHomeBinding
-import com.intive.tmdbandroid.home.ui.adapters.ScreeningPageAdapter
+import com.intive.tmdbandroid.home.ui.adapters.WatchlistAdapter
 import com.intive.tmdbandroid.home.viewmodel.HomeViewModel
 import com.intive.tmdbandroid.model.Screening
 import dagger.hilt.android.AndroidEntryPoint
@@ -28,14 +27,9 @@ class WatchlistFragment : Fragment() {
     private val clickListener = { screening: Screening ->
 //        val action = HomeFragmentDirections.actionHomeFragmentDestToDetailFragment(screening.id)
 //        findNavController().navigate(action)
+        Toast.makeText(context, screening.name, Toast.LENGTH_SHORT).show()
     }
-    private val screeningPageAdapter = ScreeningPageAdapter(clickListener)
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        viewModel.popularTVShows()
-    }
+    private val watchlistAdapter = WatchlistAdapter(clickListener)
 
     override fun onResume() {
         super.onResume()
@@ -54,40 +48,9 @@ class WatchlistFragment : Fragment() {
 
         initViews(binding)
 
-        subscribePopularData(binding)
         subscribeWatchlistData(binding)
 
         return binding.root
-    }
-
-    private fun subscribePopularData(binding: FragmentHomeBinding) {
-        binding.layoutProgressbar.progressBar.visibility = View.VISIBLE
-        lifecycleScope.launchWhenStarted {
-            viewModel.uiState.collectLatest { resultTVShows ->
-                Timber.i("MAS - popular tvshows status: $resultTVShows")
-
-                when (resultTVShows) {
-                    is State.Success<PagingData<Screening>> -> {
-                        binding.layoutError.errorContainer.visibility = View.GONE
-                        binding.layoutProgressbar.progressBar.visibility = View.GONE
-
-                        screeningPageAdapter.submitData(resultTVShows.data)
-
-                        if (screeningPageAdapter.itemCount == 0) {
-                            binding.layoutEmpty.root.visibility = View.VISIBLE
-                        } else binding.layoutEmpty.root.visibility = View.GONE
-                    }
-                    is State.Error -> {
-                        binding.layoutProgressbar.progressBar.visibility = View.GONE
-                        binding.layoutError.errorContainer.visibility = View.VISIBLE
-                    }
-                    is State.Loading -> {
-                        binding.layoutProgressbar.progressBar.visibility = View.VISIBLE
-                        binding.layoutError.errorContainer.visibility = View.GONE
-                    }
-                }
-            }
-        }
     }
 
     private fun subscribeWatchlistData(binding: FragmentHomeBinding) {
@@ -97,7 +60,7 @@ class WatchlistFragment : Fragment() {
                     is State.Success<List<Screening>> -> {
                         binding.layoutError.errorContainer.visibility = View.GONE
                         binding.layoutProgressbar.progressBar.visibility = View.GONE
-                        screeningPageAdapter.refreshWatchlistAdapter(it.data)
+                        watchlistAdapter.submitList(it.data)
                     }
                     is State.Error -> {
                         binding.layoutProgressbar.progressBar.visibility = View.GONE
@@ -124,20 +87,8 @@ class WatchlistFragment : Fragment() {
             val columnCount = floor(dpWidth / scaling).toInt()
             Timber.i("MAS - columnCount: $columnCount")
 
-            val manager = GridLayoutManager(context, columnCount)
-            manager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
-                override fun getSpanSize(position: Int): Int {
-                    return when (position) {
-                        0, 1, 2 -> columnCount
-                        else -> 1
-                    }
-                }
-            }
-
-            screeningPageAdapter.widthSize = ((floor(dpWidth / columnCount) - resources.getInteger(R.integer.short_padding)) * displayMetrics.density).toInt()
-
-            layoutManager = manager
-            adapter = screeningPageAdapter
+            layoutManager = GridLayoutManager(context, columnCount)
+            adapter = watchlistAdapter
         }
     }
 }
