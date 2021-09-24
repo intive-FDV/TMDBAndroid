3950ea3207941da59461722876b12376eb4493b4
andres.seoane <andres.seoane@intive.com>
2021-09-24 15:43:31 -0300

    Added MoviesViewModel.kt Modified MoviesFragment.kt to use MoviesViewModel.kt

--- app/src/main/java/com/intive/tmdbandroid/home/ui/MoviesFragment.kt
+++ app/src/main/java/com/intive/tmdbandroid/home/ui/MoviesFragment.kt
@@ -4,18 +4,46 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.paging.PagingData
+import androidx.recyclerview.widget.GridLayoutManager
 import com.intive.tmdbandroid.R
+import com.intive.tmdbandroid.common.State
 import com.intive.tmdbandroid.databinding.FragmentMoviesBinding
+import com.intive.tmdbandroid.home.ui.adapters.ScreeningPageAdapter
+import com.intive.tmdbandroid.home.viewmodel.MoviesViewModel
+import com.intive.tmdbandroid.model.Screening
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import timber.log.Timber
+import kotlin.math.floor
+import android.os.Parcelable
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navGraphViewModels
+import androidx.recyclerview.widget.RecyclerView
 
+@AndroidEntryPoint
 class MoviesFragment : Fragment() {
+    private val viewModel: MoviesViewModel by viewModels()
+
+    private val clickListener = { screening: Screening ->
+//        val action = WatchlistFragmentDirections.actionHomeFragmentDestToTVShowDetail(tvShow.id)
+//        findNavController().navigate(action)
+        Toast.makeText(context, screening.name, Toast.LENGTH_SHORT).show()
+    }
+    private val moviePageAdapter = ScreeningPageAdapter(clickListener)
+    private lateinit var rvPopularMovies: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (savedInstanceState == null) {
+            Timber.i("MAS - instance = null")
+            viewModel.popularMovies()
+        }
     }
 
     override fun onCreateView(
@@ -27,23 +55,58 @@ class MoviesFragment : Fragment() {
         context ?: return binding.root
 
         activity?.title = getString(R.string.popular_movies)
-        //setupToolbar(binding)
+
+        initViews(binding)
+
+        subscribePopularData(binding)
 
         return binding.root
     }
 
-//    private fun setupToolbar(binding: FragmentMoviesBinding) {
-//        val navController = findNavController()
-//        val appBarConfiguration = AppBarConfiguration(navController.graph)
-//        binding.fragmentMoviesToolbar.setupWithNavController(navController, appBarConfiguration)
-//        binding.fragmentMoviesToolbar.inflateMenu(R.menu.options_menu)
-//        binding.fragmentMoviesToolbar.setOnMenuItemClickListener{
-//            binding.fragmentMoviesToolbar.findNavController().navigate(R.id.action_homeFragmentDest_to_searchFragment)
-//            true
-//        }
-//    }
+    private fun subscribePopularData(binding: FragmentMoviesBinding) {
+        binding.layoutProgressbar.progressBar.visibility = View.VISIBLE
+        lifecycleScope.launchWhenStarted {
+            viewModel.uiState.collectLatest { resultTVShows ->
+                Timber.i("MAS - popular tvshows status: $resultTVShows")
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+                when (resultTVShows) {
+                    is State.Success<PagingData<Screening>> -> {
+                        binding.layoutError.errorContainer.visibility = View.GONE
+                        binding.layoutProgressbar.progressBar.visibility = View.GONE
+
+                        moviePageAdapter.submitData(resultTVShows.data)
+
+                        if (moviePageAdapter.itemCount == 0) {
+                            binding.layoutEmpty.root.visibility = View.VISIBLE
+                        } else binding.layoutEmpty.root.visibility = View.GONE
+                    }
+                    is State.Error -> {
+                        binding.layoutProgressbar.progressBar.visibility = View.GONE
+                        binding.layoutError.errorContainer.visibility = View.VISIBLE
+                    }
+                    is State.Loading -> {
+                        binding.layoutProgressbar.progressBar.visibility = View.VISIBLE
+                        binding.layoutError.errorContainer.visibility = View.GONE
+                    }
+                }
+            }
+        }
+    }
+
+    private fun initViews(binding: FragmentMoviesBinding) {
+        rvPopularMovies = binding.rvPopularMovies
+
+        rvPopularMovies.apply {
+            val displayMetrics = context.resources.displayMetrics
+            val dpWidth = displayMetrics.widthPixels / displayMetrics.density
+            Timber.i("MAS - dpWidth: $dpWidth")
+
+            val scaling = resources.getInteger(R.integer.screening_width)
+            val columnCount = floor(dpWidth / scaling).toInt()
+            Timber.i("MAS - columnCount: $columnCount")
+
+            layoutManager = GridLayoutManager(context, columnCount)
+            adapter = moviePageAdapter
+        }
     }
 }
