49ec9b81a65088b699c099065f91be8eb249a8ca
urielgarrido <uriel.garrido@intive.com>
2021-09-24 17:50:36 -0300

    Create DetailAndSearchActivity

---
+++ app/src/main/java/com/intive/tmdbandroid/detailandsearch/ui/DetailAndSearchActivity.kt
@@ -0,0 +1,12 @@
+package com.intive.tmdbandroid.detailandsearch.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.intive.tmdbandroid.R
+
+class DetailAndSearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_and_search)
+    }
+}
