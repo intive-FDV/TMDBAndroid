42f49b08f52708dc33fca46cd03d7caef199c1c1
urielgarrido <uriel.garrido@intive.com>
2021-09-24 18:03:34 -0300

    Add Navigation to DetailAndSearch activity when I click on search icon

--- app/src/main/java/com/intive/tmdbandroid/detailandsearch/ui/DetailAndSearchActivity.kt
+++ app/src/main/java/com/intive/tmdbandroid/detailandsearch/ui/DetailAndSearchActivity.kt
@@ -2,11 +2,25 @@ package com.intive.tmdbandroid.detailandsearch.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import com.intive.tmdbandroid.R
+import com.intive.tmdbandroid.databinding.ActivityDetailAndSearchBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DetailAndSearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_and_search)
+        val binding = ActivityDetailAndSearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_detail_and_search) as NavHostFragment
+        val navController = navHost.navController
+
+        val graph = navHost.navController
+            .navInflater.inflate(R.navigation.nav_graph)
+        graph.startDestination = R.id.searchFragmentDest
+
+        navHost.navController.graph = graph
     }
 }
