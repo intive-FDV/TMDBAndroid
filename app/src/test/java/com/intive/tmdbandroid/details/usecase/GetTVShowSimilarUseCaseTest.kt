1a9f9b95e7e26a57b7b03edc6df46869abba36be
andres.seoane <andres.seoane@intive.com>
2021-10-05 15:32:24 -0300

    Added Unit tests

---
+++ app/src/test/java/com/intive/tmdbandroid/details/usecase/GetTVShowSimilarUseCaseTest.kt
@@ -0,0 +1,86 @@
+package com.intive.tmdbandroid.details.usecase
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import app.cash.turbine.test
+import com.intive.tmdbandroid.common.MainCoroutineRule
+import com.intive.tmdbandroid.model.Genre
+import com.intive.tmdbandroid.model.Network
+import com.intive.tmdbandroid.model.Screening
+import com.intive.tmdbandroid.repository.CatalogRepository
+import com.intive.tmdbandroid.usecase.GetMovieTrailer
+import com.intive.tmdbandroid.usecase.GetTVShowSimilarUseCase
+import com.intive.tmdbandroid.usecase.SearchUseCase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.ArgumentMatchers.anyInt
+import org.mockito.BDDMockito
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
+import org.mockito.junit.MockitoJUnitRunner
+import kotlin.time.ExperimentalTime
+
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
+class GetTVShowSimilarUseCaseTest {
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private val screenings = listOf(
+        Screening(
+            backdrop_path = "BACKDROP_PATH",
+            release_date = "1983-10-20",
+            genres = listOf(Genre(1, "genre1"), Genre(2, "genre2")),
+            id = 1,
+            name = "Simona la Cacarisa",
+            number_of_episodes = 5,
+            number_of_seasons = 2,
+            overview = "Simona la cacarisa, el cochiloco",
+            poster_path = "POSTER_PATH",
+            status = "Online",
+            vote_average = 10.5,
+            vote_count = 100,
+            popularity = 34.0,
+            media_type = "tv",
+            adult = false,
+            genre_ids = null,
+            video = false,
+            networks = listOf(Network("/netflixlogo.jpg", "netflix", 123, "ARG"))
+        )
+    )
+
+    private lateinit var getTVShowSimilarUseCase: GetTVShowSimilarUseCase
+    @Mock
+    private lateinit var catalogRepository: CatalogRepository
+
+    @Before
+    fun setup(){
+        getTVShowSimilarUseCase = GetTVShowSimilarUseCase(catalogRepository)
+    }
+
+    @Test
+    @ExperimentalTime
+    fun invokeTest() {
+        mainCoroutineRule.runBlockingTest {
+            BDDMockito.given(catalogRepository.getTVShowSimilar(anyInt())).willReturn(flowOf(screenings))
+
+            val expected = getTVShowSimilarUseCase(2)
+
+            expected.test {
+                Assert.assertEquals(awaitItem(), screenings)
+                awaitComplete()
+            }
+        }
+    }
+}
