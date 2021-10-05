ab45d96c45c00c5725596b812d8d23b68dec830d
andres.seoane <andres.seoane@intive.com>
2021-10-05 14:40:34 -0300

    Added both Use cases

---
+++ app/src/main/java/com/intive/tmdbandroid/usecase/GetMovieSimilarUseCase.kt
@@ -0,0 +1,11 @@
+package com.intive.tmdbandroid.usecase
+
+import com.intive.tmdbandroid.model.Movie
+import com.intive.tmdbandroid.model.Screening
+import com.intive.tmdbandroid.repository.CatalogRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetMovieSimilarUseCase @Inject constructor(private val repository: CatalogRepository) {
+    operator fun invoke(id: Int): Flow<List<Screening>> = repository.getMovieSimilar(id)
+}
