d465e074eabac6ffb38e4d6ac533bd27f814cdb2
urielgarrido <uriel.garrido@intive.com>
2021-10-05 11:57:54 -0300

    Add Timeout to Retrofit

--- app/src/main/java/com/intive/tmdbandroid/common/RetrofitHelper.kt
+++ app/src/main/java/com/intive/tmdbandroid/common/RetrofitHelper.kt
@@ -1,18 +1,24 @@
 package com.intive.tmdbandroid.common
 
 import com.intive.tmdbandroid.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import okhttp3.OkHttpClient
-
-import okhttp3.logging.HttpLoggingInterceptor
+import java.time.Duration
 
 object RetrofitHelper {
     fun getRetrofit() : Retrofit {
         val interceptor = HttpLoggingInterceptor()
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
 
-        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+        val client = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .callTimeout(Duration.ofMinutes(1))
+            .connectTimeout(Duration.ofSeconds(30))
+            .readTimeout(Duration.ofSeconds(30))
+            .writeTimeout(Duration.ofSeconds(30))
+            .build()
 
         return Retrofit.Builder()
             .baseUrl(BuildConfig.API_BASE_URL)
