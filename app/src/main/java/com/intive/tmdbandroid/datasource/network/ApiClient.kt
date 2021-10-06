2604e74b46e1b135660ef8843d70350456bb21f9
andres.seoane <andres.seoane@intive.com>
2021-10-06 09:13:50 -0300

    Resolve merge conflict

--- app/src/main/java/com/intive/tmdbandroid/datasource/network/ApiClient.kt
+++ app/src/main/java/com/intive/tmdbandroid/datasource/network/ApiClient.kt
@@ -33,7 +33,7 @@ interface ApiClient {
 
     @GET("movie/{movie_id}/videos")
     suspend fun getMovieVideos(@Path("movie_id") movieID: Int,
-                              @Query("api_key") apiKey: String) : VideoEntity
+                               @Query("api_key") apiKey: String) : VideoEntity
 
     @GET("tv/{tv_id}/similar")
     suspend fun getTVShowSimilar(@Path("tv_id") tvShowID: Int,
@@ -42,7 +42,6 @@ interface ApiClient {
     @GET("movie/{movie_id}/similar")
     suspend fun getMovieSimilar(@Path("movie_id") movieID: Int,
                                 @Query("api_key") apiKey: String) : ResultMoviesEntity
-                               @Query("api_key") apiKey: String) : VideoEntity
 
     // SEARCH
 
