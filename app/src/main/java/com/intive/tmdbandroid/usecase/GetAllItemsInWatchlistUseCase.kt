1b803306b8c8c59c8351bc35c97050935f1706f4
Juan Dominguez <j.dominguez@intive.com>
2021-10-12 15:56:07 -0300

    Remove unused import and hardcoded dimensions

--- app/src/main/java/com/intive/tmdbandroid/usecase/GetAllItemsInWatchlistUseCase.kt
+++ app/src/main/java/com/intive/tmdbandroid/usecase/GetAllItemsInWatchlistUseCase.kt
@@ -3,7 +3,6 @@ package com.intive.tmdbandroid.usecase
 import com.intive.tmdbandroid.model.Screening
 import com.intive.tmdbandroid.repository.WatchlistRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
 class GetAllItemsInWatchlistUseCase @Inject constructor(private val watchlistRepository: WatchlistRepository) {
